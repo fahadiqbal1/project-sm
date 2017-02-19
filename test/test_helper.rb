c861bb7adddccb47bf45dc74d8da3b897cac7b19
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-02-19 21:49:41 +0000

    Removing some extra gems

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use! [
+  Minitest::Reporters::DefaultReporter.new(:color => true),
+  Minitest::Reporters::SpecReporter.new
+]
 
 class ActiveSupport::TestCase
   # Setup all fixtures test/fixtures/*.yml for all tests in alphabetical order.
