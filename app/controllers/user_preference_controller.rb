83b52048b79060ea66381d47ffe9bd4df8444d27
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-02-21 22:44:32 +0000

    Updated Profile page

--- app/controllers/user_preference_controller.rb
+++ app/controllers/user_preference_controller.rb
@@ -1,7 +1,6 @@
 # User Preference Controller
 class UserPreferenceController < ApplicationController
   def index
-    @user_preference = UserPreference.find_by(:user_id => current_user.id)
     @user = current_user
   end
 
