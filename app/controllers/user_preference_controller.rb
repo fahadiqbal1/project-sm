05296aa19df1b8b30f4236f65c0a4240861e4aa7
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-02-21 19:08:29 +0000

    More Profile Work

--- app/controllers/user_preference_controller.rb
+++ app/controllers/user_preference_controller.rb
@@ -2,6 +2,7 @@
 class UserPreferenceController < ApplicationController
   def index
     @user_preference = UserPreference.find_by(:user_id => current_user.id)
+    @user = current_user
   end
 
   def update
--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -17,4 +17,5 @@
 #
 class UserPreference < ApplicationRecord
   belongs_to :user
+  accepts_nested_attributes_for :user
 end
