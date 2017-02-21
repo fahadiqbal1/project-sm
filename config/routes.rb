5c029b6c0ba8c0c64482490220de05f452f61ea2
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-02-21 21:35:58 +0000

    Updated routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
   scope "(:locale)", :locale => /#{I18n.available_locales.join("|")}/ do
+    devise_for :admins, :path_names => {
+      :sign_in => ""
+    }
     devise_for :users, :controllers => {
       :sessions => "user/sessions"
-    }, :path_names => {
+    }, :path => "", :path_names => {
       :sign_in => "login",
       :sign_out => "logout",
       :password => "secret",
       :unlock => "unblock",
-      :registration => "_",
-      :sign_up => "sign_up"
-    }
+      :registration => "register",
+      :sign_up => ""
+    }, :skip => [:unlock, :confirmations]
 
     root "home#index"
 
