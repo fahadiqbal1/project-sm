3029e1258cfb73891781356413c85c67ee9b5e1a
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-03-02 11:29:32 +0000

    Adding content to static pages

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -2,11 +2,14 @@
 class StaticpageController < ApplicationController
   skip_before_action :authenticate_user!
   def ethos
+    add_breadcrumb "Ethos", ethos_path
   end
 
   def contribute
+    add_breadcrumb "Contribute", contribute_path
   end
 
   def privacy
+    add_breadcrumb "Privacy Policy", privacy_path
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,9 @@
 module ApplicationHelper
   def markdown(text)
     options = {
-      :filter_html => true,
-      :hard_wrap => true,
+      :filter_html => true, :hard_wrap => true,
       :link_attributes => { :rel => "nofollow", :target => "_blank" },
-      :space_after_headers => true,
-      :fenced_code_blocks => true
+      :space_after_headers => true, :fenced_code_blocks => true
     }
 
     extensions = {
@@ -17,7 +15,8 @@ module ApplicationHelper
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
 
-    sanitize markdown.render(text), :attributes => %w(type class data-dismiss)
+    sanitize markdown.render(text),
+             :attributes => %w(type class data-dismiss rel target href)
   end
 
   def yesno(x)
