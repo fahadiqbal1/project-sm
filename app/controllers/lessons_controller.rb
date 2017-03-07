bff1d12d2b2379c4edf87e45cd280822db95330c
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-03-07 17:57:55 +0000

    Added things

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,58 @@
+# Lessons Controller
 class LessonsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :authenticate_admin!, :except => [:show, :index]
+  before_action :set_lesson, :only => [:show, :edit, :update, :destroy]
+
+  # GET /lessons/1
+  def show
+  end
+
+  # GET /lessons/new
+  def new
+    @lesson = Lesson.new
+  end
+
+  # GET /lessons/1/edit
+  def edit
+  end
+
+  # POST /lessons
+  def create
+    @lesson = Lesson.new(lesson_params)
+
+    if @lesson.save
+      redirect_to @lesson, :notice => "Lesson was successfully created."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /lessons/1
+  def update
+    if @lesson.update(lesson_params)
+      redirect_to @lesson, :notice => "Lesson was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /lessons/1
+  def destroy
+    @lesson.destroy
+    redirect_to lessons_url, :notice => "Lesson was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def lesson_params
+    params.fetch(:lesson, {})
+  end
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,23 +2,62 @@
 class SubjectsController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :authenticate_admin!, :except => [:show, :index]
+  before_action :set_subject, :only => [:show, :edit, :update, :destroy]
 
-  add_breadcrumb "Courses", :courses_path
-
-  def new
-    @course = Course.find(params[:course_id])
-    @subject = Subject.new
-    add_breadcrumb @course.name, @course
-    add_breadcrumb "Add New"
+  # GET /subjects
+  def index
+    @subjects = Subject.all
   end
 
+  # GET /subjects/1
   def show
-    @course = Course.find(params[:course_id])
-    @subject = @course.subjects.find_by(:sequential_id => params[:id])
-    add_breadcrumb @course.name, @course
-    add_breadcrumb @subject.name, course_subject_path(
-      :course_id => @course,
-      :id => @subject
-    )
+  end
+
+  # GET /subjects/new
+  def new
+    @subject = Subject.new
+  end
+
+  # GET /subjects/1/edit
+  def edit
+  end
+
+  # POST /subjects
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      redirect_to @subject, :notice => "Subject was successfully created."
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /subjects/1
+  def update
+    if @subject.update(subject_params)
+      redirect_to @subject, :notice => "Subject was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /subjects/1
+  def destroy
+    @subject.destroy
+    redirect_to subjects_url, :notice => "Subject was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_subject
+    @subject = Subject.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
+  def subject_params
+    params.fetch(:subject, {})
   end
 end
