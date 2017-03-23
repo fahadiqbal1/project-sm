28faf3eec8c3a7d3a0102547464b30aede12e7f0
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-03-23 18:23:25 +0000

    Started adding tests

--- test/controllers/lesson_translations_controller_test.rb
+++ test/controllers/lesson_translations_controller_test.rb
@@ -1,49 +1,87 @@
 require "test_helper"
 
 class LessonTranslationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in admins(:one)
+    @course = courses(:course1)
+    @subject = subjects(:subject1)
+    @lesson = lessons(:lesson1)
     @lesson_translation = lesson_translations(:one)
   end
 
-  test "should get index" do
-    get lesson_translations_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get course_subject_lesson_lesson_translation_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get new_lesson_translation_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_course_subject_lesson_lesson_translation_url
+  #   assert_response :success
+  # end
 
-  test "should create lesson_translation" do
-    assert_difference("LessonTranslation.count") do
-      post lesson_translations_url, :params => { :lesson_translation => {} }
-    end
+  # test "should create lesson_translation" do
+  #   assert_difference("LessonTranslation.count") do
+  #     post course_subject_lesson_lesson_translation_url, :params => {
+  #       :lesson_translation => {
+  #         :lesson_id => 1,
+  #         :language_id => 1,
+  #         :content => "content"
+  #       }
+  #     }
+  #   end
 
-    assert_redirected_to lesson_translation_url(LessonTranslation.last)
-  end
+  #   assert_redirected_to course_subject_lesson_lesson_translation_url(
+  #     LessonTranslation.last
+  #   )
+  # end
 
   test "should show lesson_translation" do
-    get lesson_translation_url(@lesson_translation)
+    get course_subject_lesson_lesson_translation_url(
+      :course_id => @course,
+      :subject_id => @subject,
+      :lesson_id => @lesson,
+      :id => @lesson_translation
+    )
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_lesson_translation_url(@lesson_translation)
+    get edit_course_subject_lesson_lesson_translation_url(
+      :course_id => @course,
+      :subject_id => @subject,
+      :lesson_id => @lesson,
+      :id => @lesson_translation
+    )
     assert_response :success
   end
 
   test "should update lesson_translation" do
-    patch lesson_translation_url(@lesson_translation),
-          :params => { :lesson_translation => {} }
-    assert_redirected_to lesson_translation_url(@lesson_translation)
+    patch course_subject_lesson_lesson_translation_url(
+      :course_id => @course,
+      :subject_id => @subject,
+      :lesson_id => @lesson,
+      :id => @lesson_translation
+    ),
+          :params => { :lesson_translation => {
+            :content => "Testing"
+          } }
+    assert_redirected_to course_url(@course)
   end
 
-  test "should destroy lesson_translation" do
-    assert_difference("LessonTranslation.count", -1) do
-      delete lesson_translation_url(@lesson_translation)
-    end
+  # test "should destroy lesson_translation" do
+  #   assert_difference("LessonTranslation.count", -1) do
+  #     delete course_subject_lesson_lesson_translation_url(
+  #       :course_id => @course,
+  #       :subject_id => @subject,
+  #       :lesson_id => @lesson,
+  #       :id => @lesson_translation
+  #     )
+  #   end
 
-    assert_redirected_to lesson_translations_url
-  end
+  #   assert_redirected_to course_url(
+  #     :course_id => @course
+  #   )
+  # end
 end
